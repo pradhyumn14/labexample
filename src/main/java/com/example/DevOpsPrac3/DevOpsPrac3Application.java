91e394a7973ac5cc35180e25387e99bed6b864ef
pradhyumn14 <iampradhyumn@gmail.com>
2021-05-23 12:48:02 +0530

    commit
    first commit

---
+++ src/main/java/com/example/DevOpsPrac3/DevOpsPrac3Application.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsPrac3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsPrac3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsPrac3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevOpsPrac3/DevOpsPrac3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsPrac3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsPrac3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
